7dc2e17fb045d94eb57cf8f8b0da92e2d1875e2c
tekjar <k.teza1@gmail.com>
2018-04-24 20:23:06 +0530

    Add voxel example

--- vehicle_control/1_coaxial/controlled.py
+++ vehicle_control/1_coaxial/controlled.py
@@ -19,8 +19,11 @@ class CoaxialCopter:
         self.psi = 0
         self.psi_dot = 0
 
+        # z, psi, z_dot, psi_dot
+        self.X = np.array([0.0, 0.0, 0.0, 0.0])
+
     @property
-    def calculate_z_dot_dot(self):
+    def z_dot_dot(self):
         '''
         Calculates current verticle acceleration
         '''
@@ -30,9 +33,8 @@ class CoaxialCopter:
 
         return (Fg + Fz1 + Fz2)/self.m
 
-
     @property
-    def calculate_psi_dot_dot(self):
+    def psi_dot_dot(self):
         '''
         Calculates current angular acceleration
         '''
@@ -63,7 +65,7 @@ class CoaxialCopter:
         '''
         Advance state by dt during freefall
         '''
-        z_dot_dot = 9.81
+        z_dot_dot = 9.8
 
         #change in velocity
         delta_z_dot = z_dot_dot * dt
@@ -72,18 +74,43 @@ class CoaxialCopter:
         delta_z = self.z_dot * dt
         self.z = self.z + delta_z
 
+        psi_dot_dot = 0.0
+
+        #change in angular velocity
+        delta_psi_dot = psi_dot_dot * dt
+        self.psi_dot = self.psi_dot + delta_psi_dot
+
+        #change in angular position
+        delta_psi = self.psi_dot * dt
+        self.psi = self.psi + delta_psi
+
+    def advance_state(self, dt):
+        X_dot = np.array([
+            self.X[2],          #z_dot
+            self.X[3],          #psi_dot
+            self.z_dot_dot,
+            self.psi_dot_dot
+        ])
+
+        delta_X = X_dot * dt
+        self.X = self.X + delta_X
 
 
-t = np.linspace(0.0, 2.0, 21)
-dt = 0.1
+
+t = np.linspace(0.0,15.0,2000)
+target_z = np.cos(t) - 1
+target_z_dot_dot = -np.cos(t)
+dt = t[1] - t[0]
 
 drone = CoaxialCopter()
 z_history = []
-for i in range(t.shape[0]):
+for z_dot_dot_targ in target_z_dot_dot:
     z_history.append(drone.z)
-    drone.advance_state_uncontrollerd(dt)
+    drone.set_rotors_angular_velocities(z_dot_dot_targ, 0.0)
+    drone.advance_state(dt)
 
-plt.plot(t,z_history)
+plt.plot(t, z_history)
+plt.plot(t, target_z, 'r-')
 plt.ylabel('Z position meters')
 plt.xlabel('Time (s)')
 plt.gca().invert_yaxis()
